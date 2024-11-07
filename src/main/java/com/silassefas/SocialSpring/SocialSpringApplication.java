c04c02df477cd8187636d358b333b0c5d0c28e85
Silas Sefas <silassefas23@gmail.com>
2024-11-07 13:48:54 -0300

    Criando o projeto Maven

---
+++ src/main/java/com/silassefas/SocialSpring/SocialSpringApplication.java
@@ -0,0 +1,13 @@
+package com.silassefas.SocialSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silassefas/SocialSpring/SocialSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silassefas.SocialSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
