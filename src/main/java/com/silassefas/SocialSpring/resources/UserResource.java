d7a3d2be6c2aef6e6cd241285c60a594793debab
Silas Sefas <silassefas23@gmail.com>
2024-11-12 14:24:53 -0300

    User DTO

--- src/main/java/com/silassefas/SocialSpring/resources/UserResource.java
+++ src/main/java/com/silassefas/SocialSpring/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.silassefas.SocialSpring.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.silassefas.SocialSpring.domain.User;
+import com.silassefas.SocialSpring.dto.UserDTO;
 import com.silassefas.SocialSpring.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(usuario -> new UserDTO(usuario)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
