d1a0f21c66c6083a4a0bb63550750aa6127caa4d
Silas Sefas <silassefas23@gmail.com>
2024-11-26 09:05:30 -0300

    Find Posts By ID

---
+++ src/main/java/com/silassefas/SocialSpring/resources/PostResource.java
@@ -0,0 +1,30 @@
+package com.silassefas.SocialSpring.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.silassefas.SocialSpring.domain.Post;
+import com.silassefas.SocialSpring.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService service;
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post object = service.findById(id);
+		return ResponseEntity.ok().body(object);
+	}
+
+	
+	
+	
+}
---
+++ src/main/java/com/silassefas/SocialSpring/services/PostService.java
@@ -0,0 +1,26 @@
+package com.silassefas.SocialSpring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.silassefas.SocialSpring.domain.Post;
+import com.silassefas.SocialSpring.repository.PostRepository;
+import com.silassefas.SocialSpring.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	
+	public Post findById(String id) {
+		Optional<Post> object = repository.findById(id);
+		return object.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+		
+
+
+}
