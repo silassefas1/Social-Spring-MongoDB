4f73f02b901f4207b567509cf2d93c88fe497ccf
Silas Sefas <silassefas23@gmail.com>
2024-12-03 10:12:37 -0300

    @Query and FullSearch

--- src/main/java/com/silassefas/SocialSpring/repository/PostRepository.java
+++ src/main/java/com/silassefas/SocialSpring/repository/PostRepository.java
@@ -1,8 +1,10 @@
 package com.silassefas.SocialSpring.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.silassefas.SocialSpring.domain.Post;
@@ -11,4 +13,8 @@ import com.silassefas.SocialSpring.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: { $gte: ?1 } }, { date: { $lte: ?2 } } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minData, Date maxDate);
 }
+
--- src/main/java/com/silassefas/SocialSpring/resources/PostResource.java
+++ src/main/java/com/silassefas/SocialSpring/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.silassefas.SocialSpring.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +36,19 @@ public class PostResource {
 		return ResponseEntity.ok().body(list);
 	} 
 	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(defaultValue = "") String text,
+			@RequestParam(defaultValue = "") String minDate,
+			@RequestParam(defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min= URL.covertDate(minDate, new Date(0L));
+		Date max = URL.covertDate(maxDate, new Date());
+		
+		
+		List<Post> list = service.fullSearch(text, min,max);
+		return ResponseEntity.ok().body(list);
+	} 
 	
 	
 }
