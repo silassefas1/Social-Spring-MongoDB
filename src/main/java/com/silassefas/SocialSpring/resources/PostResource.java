602499a926ed21c46170436aaf3b21297278e41a
Silas Sefas <silassefas23@gmail.com>
2024-12-03 08:41:51 -0300

    Simple search with query methods

--- src/main/java/com/silassefas/SocialSpring/resources/PostResource.java
+++ src/main/java/com/silassefas/SocialSpring/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.silassefas.SocialSpring.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.silassefas.SocialSpring.domain.Post;
+import com.silassefas.SocialSpring.resources.util.URL;
 import com.silassefas.SocialSpring.services.PostService;
 
 @RestController
@@ -24,6 +28,12 @@ public class PostResource {
 		return ResponseEntity.ok().body(object);
 	}
 
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	} 
 	
 	
 	
--- src/main/java/com/silassefas/SocialSpring/services/PostService.java
+++ src/main/java/com/silassefas/SocialSpring/services/PostService.java
@@ -1,5 +1,6 @@
 package com.silassefas.SocialSpring.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,9 @@ public class PostService {
 		return object.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 		
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
+	}
 
 
 }
